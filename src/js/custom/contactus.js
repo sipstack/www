3e3906be7e3987af7151f3d3495d40296aa7875f
iniyanc <iniyan.chelladurai@gmail.com>
2020-12-16 16:25:13 -0500

    Addressed all comments brought up by Ralph in pull request

--- src/js/custom/contactus.js
+++ src/js/custom/contactus.js
@@ -2,24 +2,16 @@
 $("#contact-us-who").hide();
 
 //Whenever a new option is selected in "How did you hear about us?"
-$("#contact-us-options").change(function() {
+$("#contact-us-options").on("change", function() {
     //we check if it is 1 of the 3 options that requires aditional info, if so
     let selected = $("#contact-us-options").val();
     //change the placeholder for the additional info field correspondingly
     if (selected == "partner" || selected == "employee" || selected == "other"){
-        if (selected == "partner") {
-            $("#contact-us-who input").attr("placeholder", "example-partner");
-        }
-        else if (selected == "employee") {
-            $("#contact-us-who input").attr("placeholder", "example-employee");
-        }
-        else if (selected == "other") {
-            $("#contact-us-who input").attr("placeholder", "example-other");
-        }
-
+        $("#contact-us-who input").attr("placeholder", `example-${selected}`);
+       
         //and reenable it so that it can be seen
         $("#contact-us-who").show();
     }
-    //if it is not 1 of those 3 options, we hide the additional info field
+    //if it is not 1 of those 3 options, the additional info field is hidden
     else $("#contact-us-who").hide();
 });
