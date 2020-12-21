07b1262c0fd9561e7e04db47410e6513090ef165
Ralph Rouhana <rrouhana@sipstack.com>
2020-12-21 00:51:52 +0000

    Fixed bugs and updated pages.
    - Fixed gulpfile assets bug: javascript and scss files now fetched locally.
    - Fixed slick.js bug: added slick.min.js file for plugin.
    - Fixed plugin not found bug: commented script tag.
    - Reworked css for sidenav: now clearer and more concise.
    - Updated signup and contact pages.
    - Removed/combined unnessessary javascript files.

--- src/js/custom/logo-animation.js
+++ src/js/custom/logo-animation.js
@@ -1,22 +1,22 @@
-(function(scrollPos) {
-    $(window).on("scroll", () => {
-        if ($("html, body").scrollTop() > scrollPos) {
-            $(".la-block-1, .la-block-7").addClass("la-block-trans-0");
-            $(".la-block-2, .la-block-3, .la-block-6, .la-block-8").addClass("la-block-trans-n45");
-            $(".la-block-4, .la-block-5").addClass("la-block-trans-45");
-            $(".la-container").addClass("la-container-trans");
-        }
-    });
-    $(".la-container").on("mouseenter", () => {
-        $(".la-block-1, .la-block-7").removeClass("la-block-trans-0");
-        $(".la-block-2, .la-block-3, .la-block-6, .la-block-8").removeClass("la-block-trans-n45");
-        $(".la-block-4, .la-block-5").removeClass("la-block-trans-45");
-        $(".la-container").removeClass("la-container-trans");
-    });
-    $(".la-container").on("mouseleave", () => {
-        $(".la-block-1, .la-block-7").addClass("la-block-trans-0");
-        $(".la-block-2, .la-block-3, .la-block-6, .la-block-8").addClass("la-block-trans-n45");
-        $(".la-block-4, .la-block-5").addClass("la-block-trans-45");
-        $(".la-container").addClass("la-container-trans");
-    });
-}(($(".la-container").offset().top - 400)*0.96));
+(function () {
+	$(window).on("scroll", () => {
+		if ($("html, body").scrollTop() > ($(".la-container").offset().top - 400) * 0.96) {
+			$(".la-block-1, .la-block-7").addClass("la-block-trans-0");
+			$(".la-block-2, .la-block-3, .la-block-6, .la-block-8").addClass("la-block-trans-n45");
+			$(".la-block-4, .la-block-5").addClass("la-block-trans-45");
+			$(".la-container").addClass("la-container-trans");
+		}
+	});
+	$(".la-container").on("mouseenter", () => {
+		$(".la-block-1, .la-block-7").removeClass("la-block-trans-0");
+		$(".la-block-2, .la-block-3, .la-block-6, .la-block-8").removeClass("la-block-trans-n45");
+		$(".la-block-4, .la-block-5").removeClass("la-block-trans-45");
+		$(".la-container").removeClass("la-container-trans");
+	});
+	$(".la-container").on("mouseleave", () => {
+		$(".la-block-1, .la-block-7").addClass("la-block-trans-0");
+		$(".la-block-2, .la-block-3, .la-block-6, .la-block-8").addClass("la-block-trans-n45");
+		$(".la-block-4, .la-block-5").addClass("la-block-trans-45");
+		$(".la-container").addClass("la-container-trans");
+	});
+})();
