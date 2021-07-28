83129940dd04df06300a0b308a30c3daa7180fe5
iniyanc <iniyan.chelladurai@gmail.com>
2021-07-28 10:48:32 -0400

    Rectified env

--- src/js/custom/dark-mode.js
+++ src/js/custom/dark-mode.js
@@ -26,6 +26,8 @@ function updateTheme() {
 		document.body.style.setProperty("--main-background-color", "#ffffff");
 		document.body.style.setProperty("--primary-heading-color", "#383e45");
 		document.body.style.setProperty("--dynamic-text-color", "#4b505e");
+
+		console.log("light");
 	} else if (ss_test.theme == "dark") {
 		$(".theme-switcher").find(".bi-sun-fill").css("display", "flex");
 		$(".theme-switcher").find(".bi-sun").css("display", "none");
@@ -33,6 +35,8 @@ function updateTheme() {
 		document.body.style.setProperty("--main-background-color", "#383e45");
 		document.body.style.setProperty("--primary-heading-color", "#ffffff");
 		document.body.style.setProperty("--dynamic-text-color", "white");
+
+		console.log("dark");
 	}
 }
 
@@ -62,6 +66,7 @@ function toggleTheme() {
 		ss_test = getStorage("ss_test", "local");
 	}
 
+	console.log("load");
 	updateTheme();
 
 	$(".theme-switcher").on("click", toggleTheme);
