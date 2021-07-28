464b1cda21c42bf5911bb5a91e0c9b238cafd96c
iniyanc <iniyan.chelladurai@gmail.com>
2021-07-28 09:26:57 -0400

    Updated banner ad message, and modified it to stay hidden accross the session once closed

--- src/js/custom/banner-ad.js
+++ src/js/custom/banner-ad.js
@@ -1,10 +1,15 @@
 (function () {
-	$("#banner-container").on("click", function () {
-		// window.location.href = "the_link_to_go_to.html";
+	if (getStorage("banner") == "hidden") {
+		$("#banner-container").hide();
+	}
+
+	$("#banner-description").on("click", function () {
+		window.location.href = "https://www.sipstack.com/community/announcing-ucs-transition-into-open-source/";
 	});
 
 	// Hide banner when exit button is clicked
 	$("#banner-btn").on("click", function () {
+		sessionStorage.setItem("banner", "hidden");
 		$("#banner-container").hide();
 	});
 })();
