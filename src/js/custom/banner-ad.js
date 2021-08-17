532a4a487d7ae95b7343ae4e2d8be7100ba016eb
iniyanc <iniyan.chelladurai@gmail.com>
2021-08-17 16:49:13 -0400

    Updated banner ad to use relative link with anchor tag

--- src/js/custom/banner-ad.js
+++ src/js/custom/banner-ad.js
@@ -3,10 +3,10 @@
 		$("#banner-container").hide();
 	}
 
-	$("#banner-description").on("click", function () {
-		// REDIRECT LINK
-		window.open("https://www.sipstack.com/community/announcing-ucs-transition-into-open-source/", "_self").focus();
-	});
+	// $("#banner-description").on("click", function () {
+	// 	// REDIRECT LINK
+	// 	window.open("https://www.sipstack.com/community/announcing-ucs-transition-into-open-source/", "_self").focus();
+	// });
 
 	// Hide banner when exit button is clicked
 	$("#banner-btn").on("click", function () {
