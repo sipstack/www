302f9fee6a1510ffe611fb91500f98538e98d5f6
Jonathan Geller <jgeller@sipstack.com>
2022-06-13 11:23:38 -0400

    feat: added 'ssFooter' with cookie agree

---
+++ src/components/custom/footer/ssFooter.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { socialLinks } from "/@src/data/sipstack";
+</script>
+<template>
+	<FooterCC
+		text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Negare non possum. Apparet statim, quae sint officia, quae actiones."
+		color="dark"
+		:social-links="socialLinks"
+	>
+	</FooterCC>
+	<ssCookie />
+</template>
