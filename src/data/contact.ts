322ef2638464fdd8d88cd9a47c64a18ab2350d61
Jonathan Geller <jgeller@jZB.localdomain>
2022-06-23 17:21:58 -0400

    feat: added all pages

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -3,61 +3,65 @@ export const contactInfo = [
 		icon: "ph:handshake-duotone",
 		title: "Sales",
 		info: "sales@sipstack.com",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ille pellit, qui permulcet sensum voluptate.",
+		text: "To reach our sales team for new accounts or questions on pricing.",
 		color: "success",
 	},
 	{
 		icon: "ph:lifebuoy-duotone",
 		title: "Support",
 		info: "support@sipstack.com",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ille pellit, qui permulcet sensum voluptate.",
+		text: "Existing customers can contact support by email or via the Partner Portal.",
 		color: "danger",
 	},
 	{
 		icon: "ph:receipt-duotone",
 		title: "Billing",
 		info: "billing@sipstack.com",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ille pellit, qui permulcet sensum voluptate.",
+		text: "For existing customers with questions regarding invoices and payments.",
 		color: "default",
 	},
 	{
 		icon: "ph:chart-line-up-duotone",
 		title: "Investor Relations",
 		info: "invest@sipstack.com",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ille pellit, qui permulcet sensum voluptate.",
+		text: "For new groups interested in investing into SIPSTACK and our global future.",
 		color: "info",
 	},
 ];
 
 export const faq = [
 	{
-		title: "How does it work?",
-		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis est tam dissimile homini. Quae diligentissime contra Aristonem dicuntur a Chryippo. Qua tu etiam inprudens utebare non numquam. Paria sunt igitur. Idemne, quod iucunde.",
-	},
-	{
-		title: "What's included with membership?",
-		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis est tam dissimile homini. Quae diligentissime contra Aristonem dicuntur a Chryippo. Qua tu etiam inprudens utebare non numquam. Paria sunt igitur. Idemne, quod iucunde.",
-	},
-	{
-		title: "All the free API services really unlimited?",
-		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis est tam dissimile homini. Quae diligentissime contra Aristonem dicuntur a Chryippo. Qua tu etiam inprudens utebare non numquam. Paria sunt igitur. Idemne, quod iucunde.",
-	},
-	{
 		title: "How does billing work?",
 		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis est tam dissimile homini. Quae diligentissime contra Aristonem dicuntur a Chryippo. Qua tu etiam inprudens utebare non numquam. Paria sunt igitur. Idemne, quod iucunde.",
+			"Services that are activated that require billing will request Partners to add a credit card payment method to unlock capabilities. Billing & invoices is performed on the first of every month for usage for the previous month. Invoices are billed in USD by default and in CAD for Canadian Partners.",
 	},
 	{
-		title: "Can I sign up for dedicated support?",
+		title: "Do I need a credit card to create an account?",
 		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis est tam dissimile homini. Quae diligentissime contra Aristonem dicuntur a Chryippo. Qua tu etiam inprudens utebare non numquam. Paria sunt igitur. Idemne, quod iucunde.",
+			"No, because many products have a base free tier which covers service and access for most use cases, we do not require a credit card. For products and services that do incur usage charges, a card card payment method must be added in order to unlock the feature.",
 	},
 	{
-		title: "How do I upgrade my account?",
+		title: "Is there alternative payment method for heavy use cases?",
 		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis est tam dissimile homini. Quae diligentissime contra Aristonem dicuntur a Chryippo. Qua tu etiam inprudens utebare non numquam. Paria sunt igitur. Idemne, quod iucunde.",
+			"This is a common request. Partner accounts that expect to incur more thatn $10,000 USD in monthly recurring charges could contact our billing team for additional payment options.",
+	},
+	{
+		title: "How secure is SIPSTACK and its suite of products?",
+		content:
+			"Very! SIPSTACK suite of products & services we're built by an industry leader and security expert. Securing hosts managed within Partner clouds or on-premise requires additional security based on the use case for the deployment. Cloud UC does include base level security rules, iptables and access to other security products, however, due to the many different environments and ways to secure equipment, Cloud UC hosts are managed by the Partner directly and not SIPSTACK.",
+	},
+	{
+		title: "Can I sign up for dedicated / enterprise support?",
+		content: "Yes, enterprise support will be available in Q2 of 2023 and can be added from your Partner Portal. Pricing will be announced at that time.",
+	},
+	{
+		title: "How does SIPSTACK AI/ML work?",
+		content:
+			"We use various machine learning tools in different areas of the platform. We are continually learning about call patterns, and the authenticity of those calls to help provide the most accurate and reliable risk assesment to users of the system and through Smart CNAM and Whois. SIPSTACK Meta (project name) for our AI/ML is continually expanding its capabilities, ingestion rates, data & relationship comprehension.",
+	},
+	{
+		title: "Do you really think you can stop fraudulent calls?",
+		content:
+			"Yes, our core mission is to end the epidemic that is defrauding good people either through spoofing, robocalls, phishing or other means of scamming folks over the phone. Every product we release is maticulously crafted and though of to both help enhance telecommunication products, but also drive verifiable data into our AI/ML to always provide the most accurate information possible.",
 	},
 ];
