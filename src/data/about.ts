d5fec639b0ee7179b0d40564c11b9edfe4fea37b
Jonathan Geller <jgeller@sipstack.com>
2022-07-25 12:43:10 -0400

    stash

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,188 +1,188 @@
 export const iconValues = [
-	{
-		icon: "ion:people-outline",
-		title: "We are human",
-		text: "Products & services we develop are built with people in mind. We need tools that are easy, yet capable, attractive but complex, secure & meaninful. Eradicating telecom fraud helps all our future.",
-		color: "purple",
-	},
-	{
-		icon: "ph:brain-duotone",
-		title: "We are AI responsible",
-		text: "Processing big, very big data at a global scale requires strong fundemental values instilled at the very core. Securing and maintaining privacy is at the forefront of every founding block of AI code we commit.",
-		color: "success",
-	},
-	{
-		icon: "ph:rocket-duotone",
-		title: "We are forward thinkers",
-		text: "The challenges we face today in telecommunications will continue evolve. Solutions should not be short term designed, technology should continually adapt outpacing bad actors. While our products and services advance, so do our capabilities in ending telecom fraud.",
-		color: "yellow",
-	},
-	{
-		icon: "ph:pinwheel-duotone",
-		title: "We are creative",
-		text: "Challenges are dynamic, and solutions are not static. Developing new products that are both attractive & effective that also compliments new learning streams into our AI applications take a whole lot of creativity, and fun.",
-		color: "orange",
-	},
-];
+  {
+    icon: 'ion:people-outline',
+    title: 'We are human',
+    text: 'Products & services we develop are built with people in mind. We need tools that are easy, yet capable, attractive but complex, secure & meaninful. Eradicating telecom fraud helps all our future.',
+    color: 'purple',
+  },
+  {
+    icon: 'ph:brain-duotone',
+    title: 'We are AI responsible',
+    text: 'Processing big, very big data at a global scale requires strong fundemental values instilled at the very core. Securing and maintaining privacy is at the forefront of every founding block of AI code we commit.',
+    color: 'success',
+  },
+  {
+    icon: 'ph:rocket-duotone',
+    title: 'We are forward thinkers',
+    text: 'The challenges we face today in telecommunications will continue evolve. Solutions should not be short term designed, technology should continually adapt outpacing bad actors. While our products and services advance, so do our capabilities in ending telecom fraud.',
+    color: 'yellow',
+  },
+  {
+    icon: 'ph:pinwheel-duotone',
+    title: 'We are creative',
+    text: 'Challenges are dynamic, and solutions are not static. Developing new products that are both attractive & effective that also compliments new learning streams into our AI applications take a whole lot of creativity, and fun.',
+    color: 'orange',
+  },
+]
 
 export const founders = [
-	{
-		photo: "/assets/img/team/jon-full.png",
-		name: "Jonathan Geller",
-		role: "CEO & Founder",
-		bio: "With over 2 decades in the Telecommunications industry, our founder's inspiration comes from his interactions from real people across all industries. While finding commonality in feedback, Jonathan has taken a very personal mission to delivery better technology while ending telecom fraud at various levels.",
-		socialLinks: [
-			{
-				name: "Linkedin",
-				icon: "feather:linkedin",
-				url: "/r/link?href=https://www.linkedin.com/in/jonathangeller1/",
-			},
-			// {
-			// 	name: "Twitter",
-			// 	icon: "feather:twitter",
-			// 	url: "https://twitter.com/",
-			// },
-			{
-				name: "GitHub",
-				icon: "feather:github",
-				url: "/r/link?href=https://github.com/jonathangeller",
-			},
-		],
-	},
-];
+  {
+    photo: '/assets/img/team/jon-full.png',
+    name: 'Jonathan Geller',
+    role: 'CEO & Founder',
+    bio: "With over 2 decades in the Telecommunications industry, our founder's inspiration comes from his interactions from real people across all industries. While finding commonality in feedback, Jonathan has taken a very personal mission to delivery better technology while ending telecom fraud at various levels.",
+    socialLinks: [
+      {
+        name: 'Linkedin',
+        icon: 'feather:linkedin',
+        url: '/r/link?href=https://www.linkedin.com/in/jonathangeller1/',
+      },
+      // {
+      // 	name: "Twitter",
+      // 	icon: "feather:twitter",
+      // 	url: "https://twitter.com/",
+      // },
+      {
+        name: 'GitHub',
+        icon: 'feather:github',
+        url: '/r/link?href=https://github.com/jonathangeller',
+      },
+    ],
+  },
+]
 
 export const longTeam = [
-	{
-		image: "/assets/img/team/stam.jpg",
-		name: "Stamatis Loizides",
-		position: "Chief of Staff",
-		bio: "Stam lived and worked in over ten different countries. He creates processes and procedures to ensure everything within SIPSTACK runs as smoothly as possible and maintains the overall day-to-day of the organization. Stam has an MBA.",
-		socialLinks: [
-			{
-				name: "Linkedin",
-				icon: "feather:linkedin",
-				url: "/r/link?href=https://www.linkedin.com/in/stamloizides/",
-			},
+  {
+    image: '/assets/img/team/stam.jpg',
+    name: 'Stamatis Loizides',
+    position: 'Chief of Staff',
+    bio: 'Stam lived and worked in over ten different countries. He creates processes and procedures to ensure everything within SIPSTACK runs as smoothly as possible and maintains the overall day-to-day of the organization. Stam has an MBA.',
+    socialLinks: [
+      {
+        name: 'Linkedin',
+        icon: 'feather:linkedin',
+        url: '/r/link?href=https://www.linkedin.com/in/stamloizides/',
+      },
 
-			{
-				name: "GitHub",
-				icon: "feather:github",
-				url: "/r/link?href=https://github.com/sloizides",
-			},
-		],
-	},
-	{
-		image: "/assets/img/team/errol.png",
-		name: "Errol Geller",
-		position: "Chief Financial Officer",
-		bio: "Errol loves taking on challenges and is very good at math. He has several decades of experience as a Senior Risk Management Executive at IBM. Errol is instrumental in getting SIPSTACK to where it is today.",
-		socialLinks: [
-			{
-				name: "Linkedin",
-				icon: "feather:linkedin",
-				url: "/r/link?href=https://www.linkedin.com/in/errolgeller/",
-			},
-		],
-	},
-	{
-		image: "/assets/img/team/elon.jpeg",
-		name: "Elon Spielberg",
-		position: "Lead Clouc UC Manager",
-		bio: "With over a decade in the telecommunication industry, Elon is one of the fastest thinkers and problem solvers in the business. Our Cloud UC  team could not be in more capable hands with his guidance. ",
-		socialLinks: [
-			{
-				name: "GitHub",
-				icon: "feather:github",
-				url: "/r/link?href=https://github.com/espielberg",
-			},
-		],
-	},
-	{
-		image: "/assets/img/team/ralph.jpg",
-		name: "Ralph Rouhana",
-		position: "Lead Software Engineer",
-		bio: "Pursuing a computer engineering degree from the prestigious University of Waterloo, Ralph has been building software since he was 12 and a contiued leader at SIPSTACK. We have yet to see a problem that Ralph can not solve.",
-		socialLinks: [
-			{
-				name: "Linkedin",
-				icon: "feather:linkedin",
-				url: "/r/link?href=https://www.linkedin.com/in/ralph-rouhana-b23aa31a8/",
-			},
+      {
+        name: 'GitHub',
+        icon: 'feather:github',
+        url: '/r/link?href=https://github.com/sloizides',
+      },
+    ],
+  },
+  {
+    image: '/assets/img/team/errol.png',
+    name: 'Errol Geller',
+    position: 'Chief Financial Officer',
+    bio: 'Errol loves taking on challenges and is very good at math. He has several decades of experience as a Senior Risk Management Executive at IBM. Errol is instrumental in getting SIPSTACK to where it is today.',
+    socialLinks: [
+      {
+        name: 'Linkedin',
+        icon: 'feather:linkedin',
+        url: '/r/link?href=https://www.linkedin.com/in/errolgeller/',
+      },
+    ],
+  },
+  {
+    image: '/assets/img/team/elon.jpeg',
+    name: 'Elon Spielberg',
+    position: 'Lead Clouc UC Manager',
+    bio: 'With over a decade in the telecommunication industry, Elon is one of the fastest thinkers and problem solvers in the business. Our Cloud UC  team could not be in more capable hands with his guidance. ',
+    socialLinks: [
+      {
+        name: 'GitHub',
+        icon: 'feather:github',
+        url: '/r/link?href=https://github.com/espielberg',
+      },
+    ],
+  },
+  {
+    image: '/assets/img/team/ralph.jpg',
+    name: 'Ralph Rouhana',
+    position: 'Lead Software Engineer',
+    bio: 'Pursuing a computer engineering degree from the prestigious University of Waterloo, Ralph has been building software since he was 12 and a contiued leader at SIPSTACK. We have yet to see a problem that Ralph can not solve.',
+    socialLinks: [
+      {
+        name: 'Linkedin',
+        icon: 'feather:linkedin',
+        url: '/r/link?href=https://www.linkedin.com/in/ralph-rouhana-b23aa31a8/',
+      },
 
-			{
-				name: "GitHub",
-				icon: "feather:github",
-				url: "/r/link?href=https://github.com/ralphr123",
-			},
-		],
-	},
-];
+      {
+        name: 'GitHub',
+        icon: 'feather:github',
+        url: '/r/link?href=https://github.com/ralphr123',
+      },
+    ],
+  },
+]
 
 export const gridTeam = [
-	{
-		image: "/assets/img/team/iniyan.jpg",
-		name: "Iniyan Chelladurai",
-		position: "Junior Software Engineer",
-		socialLinks: [
-			{
-				name: "Linkedin",
-				icon: "feather:linkedin",
-				url: "/r/link?href=https://www.linkedin.com/in/iniyan-chelladurai-67307a194/",
-			},
+  {
+    image: '/assets/img/team/iniyan.jpg',
+    name: 'Iniyan Chelladurai',
+    position: 'Junior Software Engineer',
+    socialLinks: [
+      {
+        name: 'Linkedin',
+        icon: 'feather:linkedin',
+        url: '/r/link?href=https://www.linkedin.com/in/iniyan-chelladurai-67307a194/',
+      },
 
-			{
-				name: "GitHub",
-				icon: "feather:github",
-				url: "/r/link?href=https://github.com/iniyuh",
-			},
-		],
-	},
-	{
-		image: "/assets/img/team/kanasu.png",
-		name: "Kanasu Nagathihalli",
-		position: "Technical Writer",
-		socialLinks: [
-			{
-				name: "GitHub",
-				icon: "feather:github",
-				url: "/r/link?href=https://github.com/kxtkrvn",
-			},
-		],
-	},
+      {
+        name: 'GitHub',
+        icon: 'feather:github',
+        url: '/r/link?href=https://github.com/iniyuh',
+      },
+    ],
+  },
+  {
+    image: '/assets/img/team/kanasu.png',
+    name: 'Kanasu Nagathihalli',
+    position: 'Technical Writer',
+    socialLinks: [
+      {
+        name: 'GitHub',
+        icon: 'feather:github',
+        url: '/r/link?href=https://github.com/kxtkrvn',
+      },
+    ],
+  },
 
-	{
-		image: "/assets/img/team/terry.png",
-		name: "Terry Wu",
-		position: "Product (Sales) Development",
-		socialLinks: [
-			{
-				name: "GitHub",
-				icon: "feather:github",
-				url: "/r/link?href=https://github.com",
-			},
-		],
-	},
-];
+  {
+    image: '/assets/img/team/terry.png',
+    name: 'Terry Wu',
+    position: 'Product (Sales) Development',
+    socialLinks: [
+      {
+        name: 'GitHub',
+        icon: 'feather:github',
+        url: '/r/link?href=https://github.com',
+      },
+    ],
+  },
+]
 
 export const press = [
-	{
-		id: 0,
-		logo: "/assets/press/techcrunch.svg",
-		darkLogo: "/assets/press/techcrunch-dark.svg",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa tamen simplicia, vestra versuta. Negat esse eam, inquit, propter se expetendam. Quid sequatur, quid repugnet, vident. Quod quidem iam fit etiam in Academia. ",
-	},
+  {
+    id: 0,
+    logo: '/assets/press/techcrunch.svg',
+    darkLogo: '/assets/press/techcrunch-dark.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa tamen simplicia, vestra versuta. Negat esse eam, inquit, propter se expetendam. Quid sequatur, quid repugnet, vident. Quod quidem iam fit etiam in Academia. ',
+  },
 
-	{
-		id: 2,
-		logo: "/assets/press/tnw.svg",
-		darkLogo: "/assets/press/tnw.svg",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa tamen simplicia, vestra versuta. Negat esse eam, inquit, propter se expetendam. Quid sequatur, quid repugnet, vident. Quod quidem iam fit etiam in Academia. ",
-	},
+  {
+    id: 2,
+    logo: '/assets/press/tnw.svg',
+    darkLogo: '/assets/press/tnw.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa tamen simplicia, vestra versuta. Negat esse eam, inquit, propter se expetendam. Quid sequatur, quid repugnet, vident. Quod quidem iam fit etiam in Academia. ',
+  },
 
-	{
-		id: 4,
-		logo: "/assets/press/theverge.svg",
-		darkLogo: "/assets/press/theverge.svg",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa tamen simplicia, vestra versuta. Negat esse eam, inquit, propter se expetendam. Quid sequatur, quid repugnet, vident. Quod quidem iam fit etiam in Academia. ",
-	},
-];
+  {
+    id: 4,
+    logo: '/assets/press/theverge.svg',
+    darkLogo: '/assets/press/theverge.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa tamen simplicia, vestra versuta. Negat esse eam, inquit, propter se expetendam. Quid sequatur, quid repugnet, vident. Quod quidem iam fit etiam in Academia. ',
+  },
+]
