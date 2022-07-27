850029e43f8e5df5762ab2091200bcf357929f34
Jonathan Geller <jgeller@sipstack.com>
2022-07-27 16:04:48 -0400

    search tweaks in KB + gulp src_content for kb

--- src/pages/resources/knowledge-base/index.vue
+++ src/pages/resources/knowledge-base/index.vue
@@ -12,15 +12,8 @@ import { blocks } from "/@src/data/resources/docs"; // for search bar
 					title="Knowledge Base"
 					subtitle="Resources"
 					text="We have some nice resources to help you setup your project and get started as fast as possible."
-					:search="blocks"
+					:search="helpCenterCategories"
 				/>
-			</Container>
-		</Section>
-
-		<Section color="grey" top-narrow>
-			<Container>
-				<!-- <SectionTitle title="Categories" subtitle="Product" /> -->
-
 				<ssHelpCenterCategories :categories="helpCenterCategories" />
 			</Container>
 		</Section>
