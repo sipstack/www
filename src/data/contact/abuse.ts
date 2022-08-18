8778d45b23d8b5f45cdf9afde7f8657ed3d39843
mrouhana <mrouhana@sipstack.com>
2022-08-18 20:53:56 +0000

    feat: added abuse pages

---
+++ src/data/contact/abuse.ts
@@ -0,0 +1,37 @@
+export const abuseTypes = [
+  {
+    title: 'Spam',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed videbimus. Hoc est non dividere, sed frangere.',
+    // color: 'primary',
+    // icon: 'feather:mail',
+    link: 'abuse/spam',
+  },
+  {
+    title: 'Phishing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed videbimus. Hoc est non dividere, sed frangere.',
+    color: 'green',
+    // icon: 'feather:briefcase',
+    link: 'abuse/phishing',
+  },
+  {
+    title: 'Hackers',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed videbimus. Hoc est non dividere, sed frangere.',
+    // color: 'info',
+    // icon: 'feather:grid',
+    link: 'abuse/hackers',
+  },
+  {
+    title: 'Intrusion/Exploit attempts',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed videbimus. Hoc est non dividere, sed frangere.',
+    // color: 'purple',
+    // icon: 'feather:credit-card',
+    link: 'abuse/intrusion-exploit',
+  },
+  {
+    title: 'Other',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed videbimus. Hoc est non dividere, sed frangere.',
+    color: 'green',
+    // icon: 'feather:briefcase',
+    link: 'abuse/other',
+  },
+]
