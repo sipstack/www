c094b1721f34a5a1b73fc2c79ccff66bf384e2e3
Stam Loizides <sloizides@sipstack.com>
2022-10-24 15:36:35 -0400

    Update index.vue - added Partner portal and updated icons - commented out api - added & commented out sipguard, sipfree for future use. - added SIPSTACK UC and commented out for future use

--- src/pages/resources/docs/index.vue
+++ src/pages/resources/docs/index.vue
@@ -5,22 +5,29 @@
 // import { socialLinks } from "/@src/data-example/blocks/advanced/social";
 const topics = [
   {
-    icon: 'ph:swap-duotone',
-    iconColor: 'secondary',
+    icon: 'bi:command',
+    iconColor: 'primary',
     title: 'Partner Portal',
     text: 'Access all of our products through one centralized portal.',
     link: 'partner-portal',
   },
+    //   {
+// icon: 'arcticons:live-transcribe',
+//     iconColor: 'primary',
+//     title: 'SIPSTACK UC',
+//     text: 'Manage your unified communications offering throught one central platform!',
+//     link: 'sipstack-uc',
+//   },
   {
-    icon: 'ph:files-duotone',
-    iconColor: 'primary',
-    title: 'Whois Profile',
+    icon: 'arcticons:xiaoyuan-search',
+    iconColor: 'info',
+    title: 'Whois',
     text: 'Say goodbye to fraudsters & spammers with Whois Profiles.',
     link: 'whois',
   },
   {
-    icon: 'ph:atom-duotone',
-    iconColor: 'info',
+    icon: 'fluent:navigation-location-target-20-regular',
+    iconColor: 'yellow',
     title: 'Smart CNAM',
     text: 'Integrate intelligent call lookups into any voice network.',
     link: 'smart-cnam',
@@ -33,6 +40,20 @@ const topics = [
     text: 'Lower your operational costs with enhanced SIP Trunks.',
     link: 'sip-free',
   },
+  //   {
+// icon: 'arcticons:live-transcribe',
+//     iconColor: 'purple',
+//     title: 'SIP Scribe',
+//     text: 'Transcribe phone calls and voicemails with our AI powered service!',
+//     link: 'sip-scribe',
+//   },
+ //   {
+// icon: 'arcticons:eset-security',
+//     iconColor: 'danger',
+//     title: 'SIP Guard',
+//     text: 'Secure your communication offering with our propriety security product!',
+//     link: 'sip-scribe',
+//   },
 //   {
 // icon: 'ph:swap-duotone',
 //     iconColor: 'secondary',
