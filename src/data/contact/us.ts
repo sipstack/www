f72c77dfbf4e21f7137cd0e2f69f8b162c7c7062
Stam Loizides <sloizides@sipstack.com>
2022-11-01 16:45:36 -0400

    Update us.ts

--- src/data/contact/us.ts
+++ src/data/contact/us.ts
@@ -33,22 +33,22 @@ export const faq = [
 	{
 		title: "How does billing work?",
 		content:
-			"Services that are activated that require billing will request Partners to add a credit card payment method to unlock capabilities. Billing & invoices is performed on the first of every month for usage for the previous month. Invoices are billed in USD by default and in CAD for Canadian Partners.",
+			"Activated services that require billing will need a valid credit card to enable features and unlock capabilities. Billing and invoices are generated on the first of every month for the previous month’s usage. Invoices are billed in USD by default and in CAD for Canadian Partners.",
 	},
 	{
 		title: "Do I need a credit card to create an account?",
 		content:
-			"No, because many products have a base free tier which covers service and access for most use cases, we do not require a credit card. For products and services that do incur usage charges, a card card payment method must be added in order to unlock the feature.",
+			"No! Many products have a free basic/starter tier which covers access for most cases, and so we do not require a credit card. For products and services that do incur usage charges, a credit card payment method must be added in order to unlock the feature.",
 	},
 	{
-		title: "Is there alternative payment method for heavy use cases?",
+		title: "Are there alternative payment methods for frequent users?",
 		content:
-			"This is a common request. Partner accounts that expect to incur more thatn $10,000 USD in monthly recurring charges could contact our billing team for additional payment options.",
+			"This is a common request. Partner accounts that expect to incur more than $10,000 USD in monthly recurring charges can contact our billing team for additional payment options.",
 	},
 	{
 		title: "How secure is SIPSTACK and its suite of products?",
 		content:
-			"Very! SIPSTACK suite of products & services we're built by an industry leader and security expert. Securing hosts managed within Partner clouds or on-premise requires additional security based on the use case for the deployment. Cloud UC does include base level security rules, iptables and access to other security products, however, due to the many different environments and ways to secure equipment, Cloud UC hosts are managed by the Partner directly and not SIPSTACK.",
+			"Very! SIPSTACK suite of products & services were built by an industry leader and security expert. Securing hosts managed within Partner clouds or on-premise requires additional security based on the use case for the deployment. Cloud UC does include base level security rules, iptables and access to other security products; however, due to the many different environments and ways to secure equipment, Cloud UC hosts are managed by the Partner directly and not SIPSTACK.",
 	},
 	{
 		title: "Can I sign up for dedicated / enterprise support?",
@@ -62,6 +62,6 @@ export const faq = [
 	{
 		title: "Do you really think you can stop fraudulent calls?",
 		content:
-			"Yes, our core mission is to end the epidemic that is defrauding good people either through spoofing, robocalls, phishing or other means of scamming folks over the phone. Every product we release is maticulously crafted and though of to both help enhance telecommunication products, but also drive verifiable data into our AI/ML to always provide the most accurate information possible.",
+			"Yes, our core mission is to end the epidemic that is defrauding good people either through spoofing, robocalls, phishing or other means of scamming folks over the phone. Every product we release is meticulously crafted and though of to both help enhance telecommunication products, but also drive verifiable data into our AI/ML to always provide the most accurate information possible.",
 	},
 ];
