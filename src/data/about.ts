49856cd0b94ab0bcb486e9293b875b2d73e98875
Stam Loizides <sloizides@sipstack.com>
2022-11-01 16:35:19 -0400

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,25 +2,25 @@ export const iconValues = [
   {
     icon: 'ion:people-outline',
     title: 'We are human',
-    text: 'Products & services we develop are built with people in mind. We need tools that are easy, yet capable, attractive but complex, secure & meaninful. Eradicating telecom fraud helps all our future.',
+    text: 'Products & services we develop are built with people in mind. We need tools that are easy, yet capable, attractive but complex, secure & meaningful. Eradicating telecom fraud helps all our future.',
     color: 'purple',
   },
   {
     icon: 'ph:brain-duotone',
     title: 'We are AI responsible',
-    text: 'Processing big, very big data at a global scale requires strong fundemental values instilled at the very core. Securing and maintaining privacy is at the forefront of every founding block of AI code we commit.',
+    text: 'Processing big, very big data at a global scale requires strong fundamental values instilled at the very core. Securing and maintaining privacy is at the forefront of every founding block of AI code we commit.',
     color: 'success',
   },
   {
     icon: 'ph:rocket-duotone',
     title: 'We are forward thinkers',
-    text: 'The challenges we face today in telecommunications will continue evolve. Solutions should not be short term designed, technology should continually adapt outpacing bad actors. While our products and services advance, so do our capabilities in ending telecom fraud.',
+    text: 'The challenges we face today in telecommunications will continue evolve. Solutions should not be short term designed, technology should continually adapt, outpacing bad actors. While our products and services advance, so do our capabilities in ending telecom fraud.',
     color: 'yellow',
   },
   {
     icon: 'ph:pinwheel-duotone',
     title: 'We are creative',
-    text: 'Challenges are dynamic, and solutions are not static. Developing new products that are both attractive & effective that also compliments new learning streams into our AI applications take a whole lot of creativity, and fun.',
+    text: 'Challenges are dynamic, and solutions are not static. Developing new products that are attractive , effective and compliment new learning streams into our AI applications takes a whole lot of creativity, and fun.',
     color: 'orange',
   },
 ]
@@ -30,7 +30,7 @@ export const founders = [
     photo: '/assets/img/team/jon-full.png',
     name: 'Jonathan Geller',
     role: 'CEO & Founder',
-    bio: "With over 2 decades in the Telecommunications industry, our founder's inspiration comes from his interactions from real people across all industries. While finding commonality in feedback, Jonathan has taken a very personal mission to delivery better technology while ending telecom fraud at various levels.",
+    bio: "With over two decades in the telecommunications industry, our founder's inspiration comes from his interactions from real people across all industries. While finding commonality in feedback, Jonathan has taken a very personal mission to delivery better technology while ending telecom fraud at various levels.",
     socialLinks: [
       {
         name: 'Linkedin',
@@ -56,7 +56,7 @@ export const longTeam = [
     image: '/assets/img/team/stam.jpg',
     name: 'Stamatis Loizides',
     position: 'Chief of Staff',
-    bio: 'Stam lived and worked in over ten different countries. He creates processes and procedures to ensure everything within SIPSTACK runs as smoothly as possible and maintains the overall day-to-day of the organization. Stam has an MBA.',
+    bio: 'Stam lived and worked in over ten different countries. He creates processes and procedures to ensure everything within SIPSTACK runs as smoothly as possible and maintains the overall day-to-day of the organization.',
     socialLinks: [
       {
         name: 'Linkedin',
@@ -75,7 +75,7 @@ export const longTeam = [
     image: '/assets/img/team/errol.png',
     name: 'Errol Geller',
     position: 'Chief Financial Officer',
-    bio: 'Errol loves taking on challenges and is very good at math. He has several decades of experience as a Senior Risk Management Executive at IBM. Errol is instrumental in getting SIPSTACK to where it is today.',
+    bio: 'Errol loves taking on challenges and is very good at math. He has several decades of experience as a Senior Risk Management Executive at IBM. Errol has been instrumental in getting SIPSTACK to where it is today.',
     socialLinks: [
       {
         name: 'Linkedin',
@@ -88,7 +88,7 @@ export const longTeam = [
     image: '/assets/img/team/elon.jpeg',
     name: 'Elon Spielberg',
     position: 'Lead Clouc UC Manager',
-    bio: 'With over a decade in the telecommunication industry, Elon is one of the fastest thinkers and problem solvers in the business. Our Cloud UC  team could not be in more capable hands with his guidance. ',
+    bio: 'With over a decade in the telecommunications industry, Elon is one of the fastest thinkers and problem solvers in the business. Our Cloud UC  team could not be in more capable hands with his guidance. ',
     socialLinks: [
       {
         name: 'GitHub',
@@ -101,7 +101,7 @@ export const longTeam = [
     image: '/assets/img/team/ralph.jpg',
     name: 'Ralph Rouhana',
     position: 'Lead Software Engineer',
-    bio: 'Pursuing a computer engineering degree from the prestigious University of Waterloo, Ralph has been building software since he was 12 and a contiued leader at SIPSTACK. We have yet to see a problem that Ralph can not solve.',
+    bio: 'Pursuing a computer engineering degree from the prestigious University of Waterloo, Ralph has been building software since he was 12 and a continued leader at SIPSTACK. We have yet to see a problem that Ralph can not solve.',
     socialLinks: [
       {
         name: 'Linkedin',
