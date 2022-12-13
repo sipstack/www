504ffc0c6fc3b7efa5cdb9bf558b6e75deeb4895
Jonathan Geller <jgeller@sipstack.com>
2022-12-13 16:27:48 -0500

    feat: 2 holiday blogs

--- src/data/resources/blog/index.ts
+++ src/data/resources/blog/index.ts
@@ -1,5 +1,143 @@
 export const posts = [
   {
+    slug: 'ho-ho-holiday-scams',
+    image:
+      '/assets/img/resources/blog/20221213-ho-ho-holiday-scams/holiday-scam.png',
+    category: 'Scams',
+    tags: ['SIPSTACK'],
+    views: 0,
+    title: 'Ho Ho Holiday Scams',
+    abstract:
+      'Delivery scams refer to emails with shipping notifications that appear to be sent from legitimate mail carriers and parcel delivery services, but are actually sent from illegitimate scammers trying to steal your money.',
+    created: '14/12/2022', //dd/mm/yyyy
+    updated: '',
+    duration: 5, // minute read
+    author: {
+      name: 'Jeff Sager',
+      role: 'Author',
+      photo: '/assets/img/team/jeffsa.jpg',
+    },
+    content: `
+
+"*It's the most wonderful time of the year…*"  was a Christmas song written well before telecommunications scams came into existence! Unfortunately, these days, holiday season means scammers are out in full force, which is statistically worse for Christmas than the Grinch, Scrooge, and the mean dad from Elf combined. We here at Sipstack would like to do our part to spread some holiday cheer and make sure you're staying informed of two of the most prevalent holiday scams to look out for this holiday season. 
+
+## Delivery Scams (Fed EX, Amazon, UPS, etc.)
+Delivery scams refer to emails with shipping notifications that appear to be sent from legitimate mail carriers and parcel delivery services, but are actually sent from illegitimate scammers trying to steal your money. 
+
+The Covid-19 pandemic impacted online shopping in a big way, as it did with many industries, including [telecommunication fraud](https://www.sipstack.com/resources/blog/How-covid-19-has-impacted-fraud). And what better time for scammers to take advantage of the influx of online shopping than the holidays?  
+
+Already in 2022, the Federal Trade Commission has seen over 200,000 online shopping scam complaints, with reported losses of over $270 million[^1]. The Better Business Bureau said online shopping losses are estimated to exceed $380 million by the end of the year[^2]. Interestingly, they say the age group at the highest risk for these online scams are 18 to 24 year olds since they engage in online shopping the most. This is in stark contrast to many other scams that specifically target senior citizens, including [The Grandparent Scam](https://www.sipstack.com/resources/blog/the-growing-risk-of-the-grandparent-scam-in-canada). The point is whether you're young or old, nobody is immune to these scams, so it's always best to stay vigilant. 
+
+When online shopping and waiting for a delivery to arrive, emails with shipping notifications are to be expected. This is why scammers create fake delivery emails, which mimic authentic ones prompting you to click on a link. As soon as you click it, however, malware installs onto your computer or smartphone, and the scammer can track your internet browsing history and other personal details.
+
+Here's a real example of a fraudulent email, allegedly from UPS:
+
+"UPS tried to deliver your parcel, but nobody was home. Click here to rearrange your delivery."
+
+Here's another example, seemingly from FedEx:
+
+"Your FedEx package with tracking code XX-0000-XX00 is waiting for you. Update your delivery preferences here."
+
+And if you're an online shopping scammer, why not also take advantage of the most popular online retailer in the world? Here's an example of a delivery scam email from Amazon:
+
+"Because of COVID-19, your package is running late. We apologize. To make it up to you, here is $500 to spend at Amazon."
+
+If you're thinking that you've seen many emails like this even when you're _not_ expecting an online order, you would be correct. Essentially, scammers send out millions of these emails, especially during the holidays, just hoping that some people will be expecting packages from these companies. Of course, some are, and of those, some will be concerned about their package not arriving on time. These are the unlucky ones who are most likely to get scammed. 
+
+## Phone and SMS Delivery Scams
+Although email is the most popular communication method for delivery scams, both phone calls and SMS are often used as well. The details may vary, but the basic premise remains the same: There's been a problem with your delivery, and now you must provide your personal or financial details to solve it. If you mention on the phone that you haven't ordered anything, scammers have been known to try and convince you that the package is a gift from a relative or friend, and you need to provide these details to claim it. Honestly, not bad, scammers… These people are almost as creative as they are evil! 
+
+SMS delivery scams are no different. You will be asked to click on a URL or visit a website to fix the "delivery issue", which, you guessed it... results in malware. (Maybe I spoke too soon about their creativity...) Spammers are actually relying more and more on SMS scams because they know people are more likely to engage via text messages than phone calls. 
+
+Here are some SMS examples of delivery scams:
+
+"Earn $300 while you drive with an Amazon Flex sticker on your vehicle. Click on this link to get started."
+
+"UPS wants to gift you a MacBook Pro. Provide your details so we can ship it to you right away."
+
+"We need you to confirm a time-slot for your delivery. Click here."
+
+## Food Delivery Scams (Uber Eats, Postmates, DoorDash, etc.)
+Food delivery scams essentially operate in the same way, except that scammers are pretending to be food delivery services instead of shipping services. Similar to the aforementioned delivery scams, the fraudster has no way of knowing if you've actually ordered a pizza, sushi platter, or groceries that evening. But because so many people do so off of companies like DoorDash, Uber Eats, or Postmates, especially during the holidays, they can make an educated guess. Be on lookout for bogus email or SMS notifications that appear to be from these companies. They may pretend there is an issue with your order, or else act like you have access to some type of promotion. Here are some common examples:
+
+"There's been a problem with your Uber Eats order. Please click here to update your details."
+
+"Your food is on its way! Click here to confirm your delivery address."
+
+"You've won a $50 Uber Eats credit. Treat yourself to free food this holiday season! Claim your credit now!"
+
+If you think these links will cause malware to be downloaded onto your device, you are catching on and are now officially _at least_ as intelligent as your average scammer.  
+
+Speaking of intelligent scammers, we've all seen emails in our junk or spam folder that are fairly obvious scams. Sometimes, however, scammers are more sophisticated, and already have access to some of your personal details. When these more believable emails arrive at a time that you have a legitimate online food order on the way, even the most intelligent among us can be fooled. (I fell for one last year, and I _for sure_ fall into the "most intelligent among us" category...) The point is it's important to be extra cautious when giving out any personal information, either on the phone or online, so that scammers can't use it against you in an email that is super believable and totally would have fooled all of you too, okay?!
+
+## Overall Tips to Avoid Holiday Scams 
+- Only click a URL link in an email or text if you have verified where it came from.
+- Don't answer phone calls from unknown numbers, and if you do get a call or text from a number you don't recognize, enter it into [Whois by Sipstack](https://whois.sipstack.com/) to assess its Risk Rating Score.
+- Never provide personal information to unknown callers.
+- Never follow any requested call prompts. ("Press 1 to claim your prize.") 
+
+## How SIPSTACK Fights Fraud
+Using the latest technology and machine learning, SIPSTACK's systems authenticate all activity in real time against variables that affect your security. When a carrier implements SIPSTACK's Risk Rating Score, they are able to customize a threshold for calls to pass through, based on their specific needs. At SIPSTACK we take an active role in ensuring we are building a secure and connected tomorrow. [Contact us](https://whois.sipstack.com/) today to learn how you can protect yourself from spam.
+
+### References
+<sup>1 <a href="https://www.ksnblocal4.com/2022/11/27/better-business-bureau-warns-against-holiday-scams/" class="ss-reference" target="_blank">https://www.ksnblocal4.com/2022/11/27/better-business-bureau-warns-against-holiday-scams/</a></sup>
+<sup>2 <a href="https://www.ksnblocal4.com/2022/11/27/better-business-bureau-warns-against-holiday-scams/" class="ss-reference" target="_blank">https://www.ksnblocal4.com/2022/11/27/better-business-bureau-warns-against-holiday-scams/</a></sup>
+
+
+
+`,
+  },
+
+  {
+    slug: 'how-scam-affects-different-age-groups',
+    image:
+      '/assets/img/resources/blog/20221209-how-scam-affects-different-age-groups/diffages.png',
+    category: 'SCAMS',
+    tags: ['SIPSTACK'],
+    views: 0,
+    title: 'How Scam Affects Different Age Groups',
+    abstract:
+      'It is a common misconception that seniors are disproportionate victims and targets of fraud. Fraud affects every generation differently.',
+    created: '10/12/2022', //dd/mm/yyyy
+    updated: '',
+    duration: 5, // minute read
+    author: {
+      name: 'Anjana Uthayakumaran',
+      role: 'Author',
+      photo: '/assets/img/team/anjanaa.jpg',
+    },
+    content: `
+It is a common misconception that seniors are disproportionate victims and targets of fraud. This myth is dangerous, as younger people believe they are safe from scams. However, growing up with technology or being more tech-savvy doesn't necessarily protect you from scam and fraud. People of all ages should exercise caution to prevent scam, especially amongst the most prevalent methods of scam in the [US]( https://www.sipstack.com/resources/blog/the-state-of-spam-calling-in-the-US) and [Canada](https://www.sipstack.com/resources/blog/the-state-of-spam-calling-in-canada). 
+
+## So who is the most vulnerable to fraud?  
+Fraud affects every generation differently. The Consumer Sentinel Network, managed by the **FTC** **F**ederal **T**rade **C**ommission has a unique <a href="https://public.tableau.com/app/profile/federal.trade.commission/viz/AgeandFraud/Infographic" class="ss-reference" target="_blank">interactive map</a> that visually displays how fraud affects different generations. The Consumer Sentinel is a investigative cyber tool, built on the premise of collecting  data and information, and sharing said information to increase the efficiency of law enforcement.<sup>1</sup> Data is collected by consumers, and all levels of enforcement agencies, non-profit agencies, and subject matter experts. 
+
+![graph1](/assets/img/resources/blog/20221209-how-scam-affects-different-age-groups/graph1.png)  
+<sup>Source: FTC Consumer Sentinel Network </sup>
+
+### Summary of findings:
+- Individuals between the ages of 30-39 reported the most instances of fraud. 
+- Individuals over the age of 80 reported the highest dollar value in losses. The median dollar loss is $1,800 - a significant finical loss.
+- After the age of 30, as the age of the victim increases, there are less instances of fraud reported. 
+- As the age of the victim increases, there is a higher dollar value lost to fraud.
+  
+
+![graph2](/assets/img/resources/blog/20221209-how-scam-affects-different-age-groups/graph2.png)   
+<sup>Source: FTC Consumer Sentinel Network </sup>
+
+### Summary of findings:
+- Victims 70+ are most frequently contacted through phone calls. Scammers obtain money through gift cards, or alternate prepaid cards. 
+- Victims under the age of 40 are commonly tricked through social media. Money is typically collected through payment apps, credit cards and cryptocurrency. 
+- In 2022, online shopping is the most common type of fraud for individuals under the age of 80. 
+
+
+#### References 
+<sup>1 <a href="https://www.ftc.gov/enforcement/consumer-sentinel-network" class="ss-reference" target="_blank">https://www.ftc.gov/enforcement/consumer-sentinel-network</a></sup> 
+
+`,
+  },
+
+  {
     slug: 'where-does-the-term-spam-come-from',
     image:
       '/assets/img/resources/blog/20221130-where-does-the-term-spam-come-from/thinking-image.png',
