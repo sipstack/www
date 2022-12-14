378542e91032d64a923dc48057a9059737cb0e7c
Jonathan Geller <jgeller@sipstack.com>
2022-12-13 20:46:56 -0500

    feat: removed PWA plugin

--- src/utils/api/ssArticle.ts
+++ src/utils/api/ssArticle.ts
@@ -1,7 +1,7 @@
 import sleep from '/@src/utils/sleep'
 export async function getArticle(slug: string, slug_cat: string, categories) {
   const categoryFiltered = categories.find(function (data) {
-    console.log('tets')
+    // console.log('tets')
     if (data.slug == slug_cat) return true
   })
   // console.log(categoryFiltered); // debug
