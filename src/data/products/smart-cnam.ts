6a056ff786105fc64b15a4434c2444d386597e6d
Stam Loizides <sloizides@sipstack.com>
2023-01-07 20:23:18 -0500

    Update smart-cnam.ts Added billing FAQ's

--- src/data/products/smart-cnam.ts
+++ src/data/products/smart-cnam.ts
@@ -51,9 +51,44 @@ export const faq = {
         'Smart CNAM is a next generation lookup tool. Smart CNAM goes way beyond just a name response and can include a tremendous amount of information when requested.',
     },
     {
-      title: 'How do I add funds to my account?',
+      title: 'What payment methods are accepted?',
       content:
-        'You can add funds to your account through the SIPSTACK Partner Portal.',
+       'Credit cards issued by Visa, MasterCard, or American Express, Electronic Funds Transfer (EFT), Wire Transfer (for qualifying customers) or Cheque',
+    },
+    {
+      title: 'Do I need a credit card to sign up?',
+      content:
+       'Yes, a valid credit card is required.',
+    },
+    {
+      title: 'Will I receive an invoice?',
+      content:
+       'Yes, an invoice is issued on the 1st day of the month with the previous month’s subscription fee and usage. When you join during a month the subscription fee invoiced on the 1st of the month will be for the full subscription amount.',
+    },
+    {
+      title: 'How do included monthly API requests get allotted?',
+      content:
+       'At the beginning of every month, your allotted API requests for that month are based on your subscription. If you exceed the allotted requests, each additional request will be charged at the rate based on your subscription and volume. My subscription did not start on the first day of the month, how do included monthly API requests get allotted now? Even though you start during the month your allotted API requests are still based on the API’s per the subscription. If you exceed the allotted requests, each additional request will be charged at the rate based on your subscription and volume. In the event, you hardly used your allocation limit contact billing@sipstack.com and request an adjustment to your subscription fee.',
+    },
+    {
+      title: 'How do included monthly API requests get allotted?',
+      content:
+       'At the beginning of every month, your allotted API requests for that month are based on your subscription. If you exceed the allotted requests, each additional request will be charged at the rate based on your subscription and volume. My subscription did not start on the first day of the month, how do included monthly API requests get allotted now? Even though you start during the month your allotted API requests are still based on the API’s per the subscription. If you exceed the allotted requests, each additional request will be charged at the rate based on your subscription and volume. In the event, you hardly used your allocation limit contact billing@sipstack.com and request an adjustment to your subscription fee.',
+    },
+    {
+      title: 'Is there a usage limit beyond my allotted API requests?',
+      content:
+       'Yes and No. You can go over your allotted API requests, however, once you exceed your designated credit limit, you will no longer be able to make additional requests unless you upgrade to a higher plan or contact billing@sipstack.com and authorize them to take an additional payment (specify the amount) from your credit card.',
+    },
+    {
+      title: 'What is my credit limit?',
+      content:
+       'In the beginning until there is a history on the account the credit limit will be twice your subscription fee. In other words, once the number of API requests meet twice the API requests of your subscription no further API requests will be processed. We are doing this so that you are not surprised at the end of the month with a larger than expected invoice. It is important to sign up knowing the number of API requests in your system each month to avoid constantly exceeding your credit limit. However, if you want to test our system with a lower subscription fee this is okay, but you need to remember that when you see your API requests approaching double the subscription amount then you might want to upgrade to a higher or contact billing@sipstack.com to authorize a payment from your credit card to keep the requests flowing or wait until next month to resume your requests. For customers with more than 2,000,000 monthly API requests please contact billing@sipstack.com. After a history is established the credit limit will be based on several factors such as your current subscription, your cumulative spend with SIPSTACK and longevity of your account.',
+    },
+    {
+      title: 'My API requests are not going through?',
+      content:
+        'You may have reached your credit limit. If your account has been paid and you have not exceeded your credit limit and API requests are not being processed contact Support. If you feel there has been a mistake, please contact billing@sipstack.com.',
     },
     {
       title: 'How accurate is the information returned?',
@@ -92,5 +127,5 @@ export const faq = {
 
 export const cta = {
   title: 'Smarter CNAM Begins Here',
-  subtitle: 'Credit card not required to sign up.',
+  //subtitle: 'Credit card not required to sign up.',//
 }
